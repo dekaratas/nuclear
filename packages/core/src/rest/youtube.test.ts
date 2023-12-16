7792e465d840b2a0ff63cd7eefb2b30063831e36
dekaratas <-C>
2023-12-16 21:52:54 +0100

    Implement test for undefined artist yt queries

--- packages/core/src/rest/youtube.test.ts
+++ packages/core/src/rest/youtube.test.ts
@@ -5,6 +5,21 @@ const playlistLessThan100 = 'https://www.youtube.com/watch?v=TKYsuU86-DQ&list=PL
 
 const playlistGreaterThan100 = 'https://www.youtube.com/playlist?list=PLuUrokoVSxlcgocBXbDF76yWd3YKWpOH9';
 
+const mockSongOne = {
+  artist: 'Undefined',
+  track: 'Dio - Dansen In Jou live op Lowlands 2012'
+};
+
+const mockSongTwo = {
+  artist: undefined,
+  track: 'alt-j she she she live op lowlands 2012'
+};
+
+const mockSongThree = {
+  artist: '',
+  track: 'The Black Keys - Gold on the Ceiling'
+};
+
 describe('Youtube tests', () => {
   it('should able to get playlist less than 100 tracks', async () => {
     const tracks = await youtubeService.handleYoutubePlaylist(playlistLessThan100);
@@ -30,6 +45,20 @@ describe('Youtube tests', () => {
     expect(oneTrack.streams.length).toEqual(1);
     expect(oneTrack.streams[0].id.length).not.toEqual(0);
     expect(oneTrack.streams[0].source).toBe('Youtube');
+  });
+
+  it('should run search queries without error when artist is not given or Undefined', async () => {
+    let errorOccurrence = false;
+
+    try {
+      const resultOne = await youtubeService.trackSearchByString(mockSongOne, 'youtube');
+      const resultTwo = await youtubeService.trackSearchByString(mockSongTwo, 'youtube');
+      const resultThree = await youtubeService.trackSearchByString(mockSongThree, 'youtube');
+    } catch (error) {
+      errorOccurrence = true;
+    }
+
+    expect(errorOccurrence).toBe(false);
   });
 
   it('should return empty array if the url is invalid', async () => {
